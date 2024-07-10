75ed7f9e870d5dcb81d6e47eda1162b20b9381dd
theprajwalm <prmi00003@stud.uni-saarland.de>
2024-07-10 23:05:48 +0200

    "Part3"

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -4,6 +4,36 @@ public class MainProgram {
     public static void main(String[] args) {
         // write your test code here
         int[] array = {3, 1, 5, 99, 3, 12};
+
     }
+
+    public static int smallest(int[] array){
+        int smallestValue = array[0];
+        for (int i : array) {
+            if (i < smallestValue) {
+                smallestValue = i;
+            }
+        }
+        return smallestValue;
+    }
+
+    public static int indexOfSmallest(int[] array){
+        int smallestValue = smallest(array);
+        int index = 0;
+        
+        for (int i : array) {
+            if (smallestValue == i) {
+                break;
+                
+            }else{
+                index += 1;
+            }
+        }
+        return index;
+    }
+
+    // public static int indexOfSmallestFrom(int[] table, int startIndex){
+
+    // }
 
 }
