a4d5da0bc04887ee9f6f00437ec4973ad087f9aa
theprajwalm <prmi00003@stud.uni-saarland.de>
2024-07-12 22:40:36 +0200

    .

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -14,6 +14,21 @@ public class NumbersFromAFile {
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
 
+        try (Scanner readFile = new Scanner(Paths.get(file))){
+            int numbers = 0;
+            
+            while (readFile.hasNextLine()) {
+                int number = Integer.valueOf(readFile.nextLine());
+                if (number >= lowerBound && number <= upperBound) {
+                    numbers += 1;
+                }
+            }
+            System.out.println("Numbers: " + numbers);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e);
+        }
+        
     }
 
 }
